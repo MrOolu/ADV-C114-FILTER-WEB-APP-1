2c2c8f6032b537f05c4b27082936a3131bd2ec50
MrOolu <88264693+MrOolu@users.noreply.github.com>
2022-04-30 13:51:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,30 @@ function setup()
     video.size(300, 300);
     video.hide();
 
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw()
     {
 
     }
+
+    function modelLoaded()
+{
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
+    }
+}
 
     function take_snapshot()
     {
